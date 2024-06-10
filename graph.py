73db653cc0327482073e468db6df963360d41e48
kevincms <kevincms314@naver.com>
2024-06-10 22:44:52 +0900

    modified iamge path

--- graph.py
+++ graph.py
@@ -3,14 +3,14 @@ from template import html
 import matplotlib.pyplot as plt
 
 def application(environ, start_response):
-    if environ['PATH_INFO'] == '/graph.png':
+    if environ['PATH_INFO'] == '/img/graph.png':
         try:
             with open('graph.png', 'rb') as f:
                 response_body = f.read()
         except:
             response_body = ''
         start_response('200 OK', [
-            ('Content-Type', 'image/png'),
+            ('Content-Type', '/img/image/png'),
             ('Content-Length', str(len(response_body)))
         ])
         return [response_body]
